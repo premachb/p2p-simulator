bc88f4ee409806175337c71d9d4b36b30a6bd0cc
Brien Premachandiran <premachb@gmail.com>
2024-08-02 14:49:37 -0400

    Fix layout a bit for logging so it's side by side and scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,28 @@ function ServerList() {
   }
 
   return (
-    <div className="p-5">
-      <button onClick={() => setSimulationStarted(!simulationStarted)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 mr-2">Toggle Simulation (Start or Stop)</button> 
-      <button onClick={() => resetSimulation() } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">Reset Simulation</button>
-      {servers.map((server) =>
-        <Server key={server.id} serverId={server.id} chunks={server.chunks} />
-      )}
-      <h1>Logs:</h1>
-      {
-        logs.map((log, index) => (
-          <div key={index} className="bg-gray-200 p-2 mt-2">
-            {log}
-          </div>
-        ))
-      }
+    <div className="p-5 flex flex-col">
+      <div>
+        <button onClick={() => setSimulationStarted(!simulationStarted)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 mr-2">Toggle Simulation (Start or Stop)</button> 
+        <button onClick={() => resetSimulation() } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">Reset Simulation</button>
+      </div>
+      <div className="flex flex-row">
+        <div>
+          {servers.map((server) =>
+            <Server key={server.id} serverId={server.id} chunks={server.chunks} />
+          )}
+        </div>
+        <h1>Logs:</h1>
+        <div className="border p-10 overflow-scroll overflow-y-scroll max-h-80">
+          {
+            logs.map((log, index) => (
+              <div key={index} className="bg-gray-200 p-2 mt-2">
+                {log}
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
